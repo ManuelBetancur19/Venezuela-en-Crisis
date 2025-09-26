8ac5721292605daf65bbe7f1376cb5d896a01e2c
Santiago <santiago.betancur65860@ucaldas.edu.co>
2025-09-26 15:18:19 -0500

    coja oficio

---
+++ Circle.java
@@ -0,0 +1,22 @@
+public class Circle extends Shape{
+
+    private double radio;
+
+    public Circle (int id, float x, float y){
+        super(id, x, y);
+    }
+
+    public void setRadio(float radio){
+        this.radio=radio;
+    }
+
+    public double getRadio(){
+        return radio;
+    }
+
+    @Override
+    public double getArea(){
+        return(Math.PI*(radio*radio));
+    }
+
+}
